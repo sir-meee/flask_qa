45cf4f3158f495e54f4be7a336a532e3c363ddd9
sir-meee <sammymbevi@gmail.com>
2021-04-28 13:27:16 +0300

    add models

---
+++ flask_qa/models.py
@@ -0,0 +1,16 @@
+from .extensions import db 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+    expert = db.Column(db.Boolean)
+    admin = db.Column(db.Boolean)
+
+  
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text)
+    answer = db.Column(db.Text)
+    asked_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    expert_id = db.Column(db.Integer, db.ForeignKey('user.id'))
